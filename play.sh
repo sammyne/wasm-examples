8357e23338edacf3f40c6f62ca92044651eea74f
sammyne <xiangminli@alumni.sjtu.edu.cn>
2024-05-15 05:16:21 +0000

    feat(module/abi): demo returning struct

--- play.sh
+++ play.sh
@@ -2,4 +2,11 @@
 
 repo_tag=sammyne/wasm-examples-playground:149588b
 
-docker run -it --rm -v $PWD:/workspace -w /workspace $repo_tag bash
+docker run -it --rm                             \
+  -e CARGO_HOME=/root/.cargo                    \
+  -v $PWD:/workspace                            \
+  -v $PWD/_cargo/registry:/root/.cargo/registry \
+  -v $PWD/_cargo/git:/root/.cargo/git           \
+  -w /workspace                                 \
+  $repo_tag                                     \
+    bash
