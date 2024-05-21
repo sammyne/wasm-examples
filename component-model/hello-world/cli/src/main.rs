73cfbbc70bbd09c1ae90b1c5471d6ad797d6cf2b
sammyne <xiangminli@alumni.sjtu.edu.cn>
2024-05-21 08:17:40 +0000

    chore(component-model/hello-world): tidy up

--- component-model/hello-world/cli/src/main.rs
+++ component-model/hello-world/cli/src/main.rs
@@ -1,7 +1,45 @@
-mod helloworld;
-
-use clap::Parser;
 use std::path::PathBuf;
+
+use anyhow::Context;
+use clap::Parser;
+use wasmtime::component::*;
+use wasmtime::{Config, Engine, Store};
+use wasmtime_wasi::WasiCtxBuilder;
+
+wasmtime::component::bindgen!({
+    path: "../component/wit/world.wit",
+    world: "example",
+    async: true
+});
+
+#[async_std::main]
+async fn main() -> anyhow::Result<()> {
+    let App { path } = App::parse();
+
+    let mut config = Config::default();
+    config.wasm_component_model(true);
+    config.async_support(true);
+    let engine = Engine::new(&config)?;
+    let mut linker = Linker::new(&engine);
+
+    // Add the command world (aka WASI CLI) to the linker
+    wasmtime_wasi::add_to_linker_async(&mut linker).context("Failed to link command world")?;
+    let ctx = WasiCtxBuilder::new().inherit_stdout().build_p1();
+    let mut store = Store::new(&engine, ctx);
+
+    let component = Component::from_file(&engine, path).context("Component file not found")?;
+
+    let (instance, _) = Example::instantiate_async(&mut store, &component, &linker)
+        .await
+        .context("Failed to instantiate the example world")?;
+    let msg = instance
+        .call_hello_world(&mut store)
+        .await
+        .context("Failed to call add function")?;
+    println!("msg = {msg}");
+
+    Ok(())
+}
 
 /// A CLI for executing WebAssembly components that
 /// implement the `example` world.
@@ -10,18 +48,5 @@ use std::path::PathBuf;
 struct App {
     /// The path to the component.
     #[clap(value_name = "COMPONENT_PATH")]
-    component: PathBuf,
-}
-
-impl App {
-    async fn run(self) -> anyhow::Result<()> {
-        let msg = helloworld::hello_world(self.component).await?;
-        println!("{msg}");
-        Ok(())
-    }
-}
-
-#[async_std::main]
-async fn main() -> anyhow::Result<()> {
-    App::parse().run().await
+    path: PathBuf,
 }
