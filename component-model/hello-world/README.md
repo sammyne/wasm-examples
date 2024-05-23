50c1a16ef218dc9c8af64e60ba60d4330c57dbe3
sammyne <xiangminli@alumni.sjtu.edu.cn>
2024-05-23 06:10:32 +0000

    feat((component-model): demo tinygo-based component

--- component-model/hello-world/README.md
+++ component-model/hello-world/README.md
@@ -2,14 +2,15 @@
 
 ## 快速开始
 
-### 1. 编译
+### rust 语言版
+#### 1. 编译
 ```bash
 cd component
 cargo component build --release --lib
 cd -
 ```
 
-### 2. 运行
+#### 2. 运行
 
 ```bash
 cd cli
@@ -18,5 +19,31 @@ cargo run -- ../../../target/wasm32-wasi/release/hello_world.wasm
 cd -
 ```
 
+### go 语言版
+#### 1. 编译
+```bash
+cd component-go
+
+# 生成衔接代码
+bash cli.sh bindgen
+
+# 编译生成 wasm 模块代码
+bash cli.sh build
+
+# 模块组件化
+bash cli.sh
+
+cd -
+```
+
+#### 2. 运行
+```bash
+cd cli
+# hello_world.wasm 是上一步的编译产物
+cargo run -- ../component-go/out/hello-world.component.wasm
+cd -
+```
+
 ## 参考文献
 - https://component-model.bytecodealliance.org/language-support/rust.html
+- https://tinygo.org/docs/guides/webassembly/wasi/
--- play.sh
+++ play.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-repo_tag=sammyne/wasm-examples-playground:149588b
+repo_tag=sammyne/wasm-examples-playground:15f8918
 
-docker run -it --rm                             \
+name=github-wasm-examples-playground
+
+docker stop $name
+
+docker run -td --rm                             \
   -e CARGO_HOME=/root/.cargo                    \
+  --name $name                                  \
   -v $PWD:/workspace                            \
   -v $PWD/_cargo/registry:/root/.cargo/registry \
   -v $PWD/_cargo/git:/root/.cargo/git           \
   -w /workspace                                 \
   $repo_tag                                     \
     bash -c 'echo "export PATH=/root/.cargo/bin:$PATH" >> /root/.bashrc; bash'
+
+if [ -f _git/gitconfig ]; then
+  docker cp _git/gitconfig $name:/root/.gitconfig
+fi
